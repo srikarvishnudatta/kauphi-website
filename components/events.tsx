02b0a64f0d3bc1a76ae19fcdb840808e21b8ec33
venkatasrikarakella <srikarvishnudatta@gmail.com>
2025-10-20 19:23:45 -0400

    fixed apos

--- components/events.tsx
+++ components/events.tsx
@@ -1,10 +1,10 @@
-import { ArrowRight } from "lucide-react";
+ import { ArrowRight } from "lucide-react";
 import Link from "next/link";
 
 export default function Events() {
   return (
     <section className="max-w-6xl mx-auto text-center py-20 space-y-4">
-      <h2 className="text-3xl px-5">Don't want to miss us?</h2>
+      <h2 className="text-3xl px-5">Don&apos;t want to miss us?</h2>
       <p className="px-5">
         We know you are busy and we keep moving our popup all across Toronto!!
       </p>
@@ -12,7 +12,7 @@ export default function Events() {
         href={"/events"}
         className="flex justify-center items-center gap-3 bg-primary text-secondary rounded-md w-fit mx-auto px-2 py-3"
       >
-        Let's go <ArrowRight size={16} />
+        Let&apos;s go <ArrowRight size={16} />
       </Link>
     </section>
   );
