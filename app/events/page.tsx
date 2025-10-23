30fd20be0b961854b253be60f373daf339a88176
venkatasrikarakella <srikarvishnudatta@gmail.com>
2025-10-23 19:52:59 -0400

    fixed screen bug

--- app/events/page.tsx
+++ app/events/page.tsx
@@ -19,6 +19,6 @@ export default function Events() {
 
 function EventMap(){
   return <div>
-    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2887.3605141972052!2d-79.38828602395066!3d43.6406671711027!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x882b34d609c5ff9f%3A0x1fc0a7c387627fe0!2s255%20Bremner%20Blvd%2C%20Toronto%2C%20ON%20M5V%203M9!5e0!3m2!1sen!2sca!4v1761250762048!5m2!1sen!2sca" width="400" height="250"  allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade" />
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2887.3605141972052!2d-79.38828602395066!3d43.6406671711027!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x882b34d609c5ff9f%3A0x1fc0a7c387627fe0!2s255%20Bremner%20Blvd%2C%20Toronto%2C%20ON%20M5V%203M9!5e0!3m2!1sen!2sca!4v1761250762048!5m2!1sen!2sca" width="400" height="250" loading="lazy" referrerPolicy="no-referrer-when-downgrade" />
   </div>
 }
